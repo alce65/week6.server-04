351cfe3ca849024b485684cee53f73ec0164d27f
Alejandro Cerezo <alce65@hotmail.es>
2023-06-08 17:16:55 +0200

    ✨ Add http basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log('Index');
+import http from 'http';
+import url from 'url';
+import { program } from 'commander';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4444;
+const version = '1.0.0';
+
+program.option('-v, --version');
+program.parse();
+const options = program.opts();
+
+if (options.version) {
+  console.log('Version ' + version);
+  process.exit(0);
+}
+
+const server = http.createServer((req, res) => {
+  if (!req.url) {
+    server.emit('error', new Error('No url in the request'));
+    return;
+  }
+
+  const { pathname } = url.parse(req.url);
+
+  if (req.method !== 'GET') {
+    server.emit('error', new Error('Invalid method'));
+    return;
+  }
+
+  res.write(`<h1>Hola ${pathname!.toUpperCase()}</h1>`);
+  res.write(req.method);
+  res.write(req.url);
+  res.end();
+});
+
+server.listen(PORT);
+
+server.on('listening', () => {
+  console.log('Listening on port ' + PORT);
+});
+
+server.on('error', (error) => {
+  console.log(error.message);
+});
